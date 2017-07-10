290a6611cc388e13bd01843e0ff7c2ac288d2caf
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-07-09 23:50:43 -0400

    bump version

--- ExtendedFridge/Properties/AssemblyInfo.cs
+++ ExtendedFridge/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Crystalmir.StardewMods")]
 [assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: Guid("e15d4c4a-d9d7-4113-a069-40ece09ec443")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
