6875d990ed7ba48ff24de69268253af9a3e1909e
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-07-09 23:09:40 -0400

    simplify assembly info

--- ExtendedFridge/Properties/AssemblyInfo.cs
+++ ExtendedFridge/Properties/AssemblyInfo.cs
@@ -1,20 +1,10 @@
-﻿using System.Diagnostics;
-using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Runtime.Versioning;
 
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTitle("Crystalmir.ExtendedFridge")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyProduct("SDV_ShopTest")]
-[assembly: AssemblyTitle("SDV_ShopTest")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: CompilationRelaxations(8)]
-[assembly: ComVisible(false)]
-[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: AssemblyProduct("Crystalmir.StardewMods")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: Guid("e15d4c4a-d9d7-4113-a069-40ece09ec443")]
-[assembly: RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
